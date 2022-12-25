b829cbfd6d6646f96d95d8c9e394c775a4fd120e
Max! <admin@cloudydev.net>
2022-12-25 11:41:45 -0800

    Update bucket.tf

--- gcp-tf-base/bucket.tf
+++ gcp-tf-base/bucket.tf
@@ -2,10 +2,4 @@ data "google_storage_bucket" "bucket" {
   name          = var.backend_bucket_name
   location      = var.location
   project       = var.project_id
-
-  versioning {
-    enabled = true
-  }
-
-  uniform_bucket_level_access = true
 }
